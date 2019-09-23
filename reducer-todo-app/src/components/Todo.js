724505e9dd0b406d56792a2c7af6078267c496e9
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-09-22 19:33:20 -0700

    New todo items now show up and get added to the state

--- reducer-todo-app/src/components/Todo.js
+++ reducer-todo-app/src/components/Todo.js
@@ -4,12 +4,34 @@ import TodoItem from "./TodoItem";
 
 function Todo() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [todoItems, setTodoItems] = useState(state);
+  const [newTodoItem, setNewTodoItem] = useState();
 
-  const stateArray = [state];
+  const handleChanges = e => {
+    setTodoItems(e.target.value);
+    setNewTodoItem(e.target.value);
+  };
+
+  console.log(state);
 
   return (
     <div>
-      {stateArray.map(item => {
+      <input
+        // className="todo-input"
+        type="text"
+        name="todoItems"
+        value={newTodoItem}
+        onChange={handleChanges}
+      />
+      <button
+        onClick={() => {
+          dispatch({ type: "ADD_TODO", payload: newTodoItem });
+        }}
+      >
+        Add New Item
+      </button>
+      {state.map(item => {
+        console.log(item);
         return <TodoItem item={item} />;
       })}
     </div>
--- reducer-todo-app/src/reducers/reducer.js
+++ reducer-todo-app/src/reducers/reducer.js
@@ -1,12 +1,19 @@
-export const initialState = {
-  item: "Learn about reducers",
-  completed: false,
-  id: 3892987589
-};
+export const initialState = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589
+  }
+];
 
 export const reducer = (state, action) => {
-  //   switch (action.type) {
-  //       case "DISPLAY"
-  //   }
-  return state;
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        { item: action.payload, completed: false, id: new Date() }
+      ];
+    default:
+      return state;
+  }
 };
